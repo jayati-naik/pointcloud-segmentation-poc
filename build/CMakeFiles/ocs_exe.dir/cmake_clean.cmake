a9da215a040869e5d784d8bd311abf5576ac3e40
jayati-naik <naik.jayati89@gmail.com>
2021-06-15 17:31:45 -0700

    unorganized pcd Test data added

---
+++ build/CMakeFiles/ocs_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ocs_exe.dir/Unorganized_pcd/3DObjectSegmentation.cpp.o"
+  "ocs_exe"
+  "ocs_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ocs_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
